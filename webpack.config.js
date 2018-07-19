a00bdf76f4e35585d6599f3c64edf11d63b01d90
D0n0tu$e <navi.marella@gmail.com>
2018-07-18 19:52:54 -0600

    Added Babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,33 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const parts = require("./webpack.parts");
 
-const commonConfig = merge([
-    {
-      plugins: [
-        new HtmlWebpackPlugin({
-          title: "Webpack demo",
-        }),
-      ],
-    },
-    parts.loadCSS()
-  ]);
-
-
-  const productionConfig = merge([
-    parts.extractCSS({
-        use: ["css-loader", parts.autoprefix()],
+const commonConfig = merge([{
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack demo",
       }),
-  ]);
+    ],
+  },
+  parts.loadCSS(),
+  parts.loadJavaScript({
+    include: __dirname + "/src"
+  }),
+]);
+
+
+const productionConfig = merge([
+  parts.extractCSS({
+    use: ["css-loader", parts.autoprefix()],
+  }),
+  parts.loadImages({
+    options: {
+      limit: 15000,
+      name: "[name].[ext]",
+      fallback: "file-loader",
+      outputPath: "assets"
+    },
+  }),
+]);
 
 const developmentConfig = merge([
   parts.devServer({
@@ -28,13 +38,21 @@ const developmentConfig = merge([
     host: process.env.HOST,
     port: process.env.PORT,
   }),
+  parts.loadImages()
 ]);
 
 
-module.exports = ({mode}) => {
-    if (mode === "production") {
-      return merge(commonConfig, productionConfig, { mode });
-    }
+module.exports = ({
+  mode
+}) => {
+  process.env.BABEL_ENV = mode;
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, {
+      mode
+    });
+  }
 
-    return merge(commonConfig, developmentConfig, { mode });
-  };
+  return merge(commonConfig, developmentConfig, {
+    mode
+  });
+};
--- webpack.parts.js
+++ webpack.parts.js
@@ -1,38 +1,46 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-exports.devServer = ({ host, port } = {}) => ({
-    devServer: {
-      stats: "errors-only",
-      host, // Defaults to `localhost`
-      port, // Defaults to 8080
-      open: true,
-      overlay: true,
-    },
-  });
+exports.devServer = ({
+  host,
+  port
+} = {}) => ({
+  devServer: {
+    stats: "errors-only",
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+    open: true,
+    overlay: true,
+  },
+});
 
-  exports.autoprefix = () => ({
-    loader: "postcss-loader",
-    options: {
-      plugins: () => [require("autoprefixer")()],
-    },
-  });
+exports.autoprefix = () => ({
+  loader: "postcss-loader",
+  options: {
+    plugins: () => [require("autoprefixer")()],
+  },
+});
 
-  exports.loadCSS = ({ include, exclude } = {}) => ({
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          include,
-          exclude,
+exports.loadCSS = ({
+  include,
+  exclude
+} = {}) => ({
+  module: {
+    rules: [{
+      test: /\.css$/,
+      include,
+      exclude,
 
-          use: ["style-loader", "css-loader", "sass-loader"],
-        },
-      ],
-    },
-  });
+      use: ["style-loader", "css-loader", "sass-loader"],
+    }, ],
+  },
+});
 
 
-exports.extractCSS = ({ include, exclude, use = [] }) => {
+exports.extractCSS = ({
+  include,
+  exclude,
+  use = []
+}) => {
   // Output extracted CSS to a file
   const plugin = new MiniCssExtractPlugin({
     filename: "[name].css",
@@ -40,18 +48,51 @@ exports.extractCSS = ({ include, exclude, use = [] }) => {
 
   return {
     module: {
-      rules: [
-        {
-          test: /\.css$/,
-          include,
-          exclude,
+      rules: [{
+        test: /\.css$/,
+        include,
+        exclude,
 
-          use: [
-            MiniCssExtractPlugin.loader,
-          ].concat(use),
-        },
-      ],
+        use: [
+          MiniCssExtractPlugin.loader,
+        ].concat(use),
+      }, ],
     },
     plugins: [plugin],
   };
 };
+exports.loadImages = ({
+  include,
+  exclude,
+  options
+} = {}) => ({
+  module: {
+    rules: [{
+        test: /\.(png|jpg)$/,
+        include,
+        exclude,
+        use: {
+          loader: "url-loader",
+          options,
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+    ],
+  },
+});
+
+exports.loadJavaScript = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include,
+        exclude,
+        use: "babel-loader",
+      },
+    ],
+  },
+});
